6804785f0e3c27a09cc9569e11d57dcc192f8d61
Yusuf PM Pangaribuan <iss14009@students.del.ac.id>
2018-05-04 21:59:23 +0700

    simple routing

--- Simple Web File Server/app.js
+++ Simple Web File Server/app.js
@@ -3,8 +3,20 @@ var fs = require('fs');
 
 http.createServer(function(req,res)
 {
-	res.writeHead(200,{"Content_Type" : "text/html"});
-	fs.createReadStream('./template/index.html').pipe(res);
+	var kode = 0;
+	var file = "";
+	if(req.url == "/"){
+		file = "index.html";
+		kode = 200;
+	}else if(req.url == "/contact"){
+		file = "contact.html";
+		kode = 200;
+	}else{
+		file = "404.html";
+		kode = 404;
+	}
+	res.writeHead(kode,{"Content_Type" : "text/html"});
+	fs.createReadStream('./template/' + file).pipe(res);
 }).listen(8888);
 
 console.log("Server Is Running...")
